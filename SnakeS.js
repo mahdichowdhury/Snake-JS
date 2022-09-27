1652785b782d3c351cb237a5e79799453e123bde
mahdichowdhury <113201272+mahdichowdhury@users.noreply.github.com>
2022-09-27 13:13:27 +0100

    Add files via upload

---
+++ SnakeS.js
@@ -0,0 +1,16 @@
+
+//board
+var blockSize = 25;
+var rows = 20;
+var columns =20;
+var board;
+var context;
+
+window.onload =  function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = columns * blockSize;
+    context = getContext
+
+
+}
